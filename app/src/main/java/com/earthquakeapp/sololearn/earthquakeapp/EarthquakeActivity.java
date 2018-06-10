963c770662b4f6d48056144aa10840dcb8c7b7a7
Vahagn Khorotyan <khorotyanvahagn@gmail.com>
2018-06-10 17:29:04 +0400

    Show that there is no internet connection when there is not

--- app/src/main/java/com/earthquakeapp/sololearn/earthquakeapp/EarthquakeActivity.java
+++ app/src/main/java/com/earthquakeapp/sololearn/earthquakeapp/EarthquakeActivity.java
@@ -1,8 +1,11 @@
 package com.earthquakeapp.sololearn.earthquakeapp;
 
 import android.app.LoaderManager;
+import android.content.Context;
 import android.content.Intent;
 import android.content.Loader;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -55,11 +58,26 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderManag
             }
         });
 
-        // Get a reference to the LoaderManager to interact with loaders
-        LoaderManager loaderManager = getLoaderManager();
+        // Get reference to the ConnectivityManager to check state of network connectivity
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
 
-        // Initialize the loader
-        loaderManager.initLoader(EARTHQUAKE_LOADER_ID, null, this);
+        // Get details on the currently active default data networks
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+
+        // If there is a network connection, get data
+        if (networkInfo != null && networkInfo.isConnected()) {
+            // Get a reference to the LoaderManager to interact with loaders
+            LoaderManager loaderManager = getLoaderManager();
+
+            // Initialize the loader
+            loaderManager.initLoader(EARTHQUAKE_LOADER_ID, null, this);
+        } else {
+            // Hide the loading indicator so error message will be visible
+            View loadingIndicator = findViewById(R.id.loading_indicator);
+            loadingIndicator.setVisibility(View.GONE);
+
+            mEmptyStateTextView.setText("No internet connection.");
+        }
     }
 
     @Override
@@ -72,6 +90,8 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderManag
         // Hide the loading indicator as the data has been loaded
         View loadingIndicator = findViewById(R.id.loading_indicator);
         loadingIndicator.setVisibility(View.GONE);
+
+        mEmptyStateTextView.setText("No earthquakes found.");
 
         // Clear the adapter of previous earthquake data
         mAdapter.clear();
